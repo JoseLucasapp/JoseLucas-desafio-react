c5c0040fa7189f66df494d3bc6fd44e27136dbf8
José Lucas <josluc2016@gmail.com>
2022-07-30 19:39:03 -0300

    feat: repository initialized

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable indent */
+/* eslint-disable react/jsx-indent */
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            Home ucas
+        </div>
+    );
+}
